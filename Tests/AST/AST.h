7a5e7c0ec0d5a216622249fd9c467b6fea485f50
Fabrice Servais <fabrice.servais@gmail.com>
2015-03-26 13:08:55 +0100

    Begin AST

---
+++ Tests/AST/AST.h
@@ -0,0 +1,21 @@
+//
+//  AST.h
+//  AST
+//
+//  Created by Fabrice Servais on 26/03/15.
+//  Copyright (c) 2015 Fabrice Servais. All rights reserved.
+//
+
+#ifndef __AST__AST__
+#define __AST__AST__
+
+#include <stdio.h>
+
+class AST {
+    <#instance variables#>
+    
+public:
+    <#member functions#>
+};
+
+#endif /* defined(__AST__AST__) */
---
+++ Tests/AST/AST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AST
+//
+//  Created by Fabrice Servais on 26/03/15.
+//  Copyright (c) 2015 Fabrice Servais. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
