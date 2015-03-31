a9003928ea54aab72941d994227e06476f6ee867
Waliens <romainmormont@hotmail.com>
2015-03-31 08:46:23 +0200

    start token classes implementation

---
+++ compiler/ast/nodes/tokens/Token.cpp
@@ -0,0 +1,111 @@
+#include "Token.hpp"
+
+using namespace ast::nodes::tokens;
+using ast::nodes::ASTNode;
+using ast::nodes::NodeLocation;
+
+/********************
+ * Token base class *
+ ********************/
+Token::Token(const std::string& node_name) : ASTNode(node_name) {}
+
+Token::Token(const std::string& node_name, int first_line, int last_line, int first_column, int last_column) 
+	: ASTNode(node_name, first_line, last_line, first_column, last_column)
+{
+
+}
+
+Token::Token(const std::string& node_name, const NodeLocation& node_loc) 
+	: ASTNode(node_name, node_loc)
+{
+
+}
+
+/***********************************
+ * Token intermediate base classes *
+ ***********************************/
+
+/** Constant token */
+ConstantToken::ConstantToken(const std::string& node_name) : Token(node_name) {}
+
+ConstantToken::ConstantToken(const std::string& node_name, int first_line, int last_line, int first_column, int last_column)
+	: Token(node_name, first_line, last_line, first_column, last_column)
+{
+
+}
+
+ConstantToken::ConstantToken(const std::string& node_name, const NodeLocation& loc)
+	: Token(node_name, node_loc)
+{
+
+}
+
+/** Delimiter */
+Delimiter::Delimiter(const std::string& node_name) : Token(node_name) {}
+
+Delimiter::Delimiter(const std::string& node_name, int first_line, int last_line, int first_column, int last_column)
+	: Token(node_name, first_line, last_line, first_column, last_column)
+{
+
+}
+
+Delimiter::Delimiter(const std::string& node_name, const NodeLocation& loc)
+	: Token(node_name, node_loc)
+{
+
+}
+
+/** Operator */
+Operator::Operator(const std::string& node_name) : Token(node_name) {}
+
+Operator::Operator(const std::string& node_name, int first_line, int last_line, int first_column, int last_column)
+	: Token(node_name, first_line, last_line, first_column, last_column)
+{
+
+}
+
+Operator::Operator(const std::string& node_name, const NodeLocation& loc)
+	: Token(node_name, node_loc)
+{
+
+}
+
+/** Keyword */
+Keyword::Keyword(const std::string& node_name) : Token(node_name) {}
+
+Keyword::Keyword(const std::string& node_name, int first_line, int last_line, int first_column, int last_column)
+	: Token(node_name, first_line, last_line, first_column, last_column)
+{
+
+}
+
+Keyword::Keyword(const std::string& node_name, const NodeLocation& loc)
+	: Token(node_name, node_loc)
+{
+
+}
+
+/** Identifier */
+Identifier::Identifier(const std::string& id) : Token("identifier"), id_(id) {}
+
+Identifier::Identifier(const std::string& id, int first_line, int last_line, int first_column, int last_column)
+	: Token("identifier", first_line, last_line, first_column, last_column), id_(id) 
+{
+
+}
+
+Identifier::Identifier(const std::string& id, const NodeLocation& loc)
+	: Token("identifier", node_loc), id_(id)
+{
+
+}
+
+std::string& id()
+{
+	return id_;
+}
+
+const std::string& id() const
+{
+	return id_;
+}
---
+++ compiler/ast/nodes/tokens/Token.hpp
@@ -0,0 +1,84 @@
+#ifndef TOKEN_HPP_DEFINED
+#define TOKEN_HPP_DEFINED value
+
+#include <string>
+
+namespace ast::nodes::tokens
+{
+
+	/*************************
+	 * Token node base class *
+	 *************************/
+	class Token : public ast::nodes::ASTNode
+	{
+	public: 
+		// constructors
+		// string arg is the token node name 
+		Token(const std::string&);
+		// additionnal arguments are for location (see ast node constructors for ordering)
+		Token(const std::string&,int,int,int,int);
+		Token(const std::string&,const ast::nodes::NodeLocation&);
+	};
+
+	/**************************************
+	 * Intermediate token node base class *
+	 **************************************/
+	class ConstantToken : public Token
+	{
+	public: 
+		// constructors
+		ConstantToken(const std::string&);
+		ConstantToken(const std::string&,int,int,int,int);
+		ConstantToken(const std::string&,const ast::nodes::NodeLocation&);
+	};
+
+	class Operator : public Token
+	{
+	public: 
+		// constructors
+		Operator(const std::string&,);
+		Operator(const std::string&,int,int,int,int);
+		Operator(const std::string&,const ast::nodes::NodeLocation&);
+	};
+
+	class Delimiter : public Token
+	{
+	public: 
+		// constructors
+		Delimiter(const std::string&,);
+		Delimiter(const std::string&,int,int,int,int);
+		Delimiter(const std::string&,const ast::nodes::NodeLocation&);
+	};
+
+	class Keyword : public Token
+	{
+	public: 
+		// constructors
+		Keyword(const std::string&,);
+		Keyword(const std::string&,int,int,int,int);
+		Keyword(const std::string&,const ast::nodes::NodeLocation&);
+	};
+
+	class Identifier : public Token
+	{
+	public: 
+		// constructors
+		// the string parameters is the identifier name (not the node name /!\)
+		Identifier(const std::string&);
+		Identifier(const std::string&,int,int,int,int);
+		Identifier(const std::string&,const ast::nodes::NodeLocation&);
+
+		// id getters 
+		std::string& id();
+		const std::string& id() const;
+
+	private:
+		std::string id_;
+	};
+
+	/*************
+	 * Operators *
+	 *************/
+}
+
+#endif
