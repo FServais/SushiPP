cc63e0b0e60d56ad9120dce9fa2530340ce49dcf
Fabrice Servais <fabrice.servais@gmail.com>
2015-04-15 00:23:46 +0200

    ErrorItem (abstract)

---
+++ compiler/errors/ErrorItem.cpp
@@ -0,0 +1,18 @@
+#include "ErrorItem.hpp"
+
+using namespace errors;
+
+std::string ErrorItem::error_flag = "error";
+
+void ErrorItem::print_location_error() const
+{
+	std::cout << context << std::endl;
+	for(int i = 0 ; i < context.size() ; ++i)
+	{
+		if(i+1 == line)
+			std::cout << "▲";
+		else
+			std::cout << "_";
+	}
+	std::cout << std::endl;
+}
