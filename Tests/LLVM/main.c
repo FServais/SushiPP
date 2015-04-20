4ac86b20509490684142fcd4fe21dd9bd80d5db9
Fabrice Servais <fabrice.servais@gmail.com>
2015-04-20 17:13:53 +0200

    New main test

--- Tests/LLVM/main.c
+++ Tests/LLVM/main.c
@@ -1,9 +1,22 @@
-
-#include <stdio.h>
 #include <stdlib.h>
+
+static int apply(int (*) (int, int), int, int);
+int apply(int (*func) (int, int), int a , int b)
+{
+	return func(a,b);
+}
+
+static int plus(int,int);
+int plus(int a, int b)
+{
+	return a + b;
+}
 
 int main()
 {
-  int a = 5;
-	printf("%i", a);
+	int a = 1, b = 2;
+
+	int result = apply(plus, a, b);
+
+	return 0;
 }
