b3a2172a8a7e10ae68d220e2b82043ac5c05086b
Fabrice Servais <fabrice.servais@gmail.com>
2015-05-02 22:17:31 +0200

    Useless test

--- compiler/ast/visitor/testCGVisitor.cpp
+++ compiler/ast/visitor/testCGVisitor.cpp
@@ -2,13 +2,9 @@
 #include <iostream> // cerr, endl
 #include <stdexcept> // exception
 
-#include "llvm/IR/Verifier.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
+#include "CodeGenVisitor.hpp"
 
-using namespace visitor;
+using namespace codegen;
 
 int main(int argc, char** argv)
 {
