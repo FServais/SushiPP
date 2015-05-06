abdb3d499df1f70fed8c0954fb62779679bbcffc
Waliens <r.mormont@student.ulg.ac.be>
2015-05-06 13:42:29 +0200

    Add test for list runtime

--- compiler/runtime/main.c
+++ compiler/runtime/main.c
@@ -15,13 +15,82 @@ int main()
 		   id3 = list_allocate_char(table),
 		   id4 = list_allocate_bool(table);
 
-	print_list_table(table);
+	list_add_reference(table, id1);
+	list_add_reference(table, id2);
+	list_add_reference(table, id3);
+	list_add_reference(table, id4);
 
 	list_push_back_int(table, id1, 5);
 	list_push_back_int(table, id1, 3);
 	list_push_back_int(table, id1, 18);
 	list_push_back_int(table, id1, 6);
 
+	list_push_front_float(table, id2, 5.25);
+	list_push_front_float(table, id2, 3.25);
+	list_push_front_float(table, id2, 18.25);
+	list_push_front_float(table, id2, 6.25);
+
+	list_insert_element_bool(table, id4, 0, true);
+	list_insert_element_bool(table, id4, 1, false);
+	list_insert_element_bool(table, id4, 0, true);
+	list_insert_element_bool(table, id4, 2, false);
+
+	list_push_back_char(table, id3, 'a');
+	list_push_back_char(table, id3, 'b');
+	list_push_back_char(table, id3, 'c');
+	list_push_back_char(table, id3, 'd');
+	list_push_back_char(table, id3, 'e');
+	list_push_back_char(table, id3, 'f');
+	list_push_back_char(table, id3, 'g');
+	list_push_back_char(table, id3, 'h');
+	list_push_back_char(table, id3, 'i');
+	list_push_back_char(table, id3, 'j');
+	list_push_back_char(table, id3, 'k');
+	list_push_back_char(table, id3, 'l');
+	list_push_back_char(table, id3, 'a');
+
+	print_list_table(table);
+
+	printf("Front/back :\n");
+	printf("  List %zu front : %c\n", id3, list_front_char(table, id3));
+	printf("  List %zu back : %c\n", id3, list_back_char(table, id3));
+	printf("  List %zu front : %d\n", id1, list_front_int(table, id1));
+	printf("  List %zu back : %d\n", id1, list_back_int(table, id1));
+	printf("  List %zu front : %lf\n", id2, list_front_float(table, id2));
+	printf("  List %zu back : %lf\n", id2, list_back_float(table, id2));
+	printf("  List %zu front : %d\n", id4, list_front_bool(table, id4));
+	printf("  List %zu back : %d\n", id4, list_back_bool(table, id4));
+
+	printf("PopFront/PopBack :\n");
+	printf("  List %zu front : %c\n", id3, list_pop_front_char(table, id3));
+	printf("  List %zu back : %c\n", id3, list_pop_back_char(table, id3));
+	printf("  List %zu front : %d\n", id1, list_pop_front_int(table, id1));
+	printf("  List %zu back : %d\n", id1, list_pop_back_int(table, id1));
+	printf("  List %zu front : %lf\n", id2, list_pop_front_float(table, id2));
+	printf("  List %zu back : %lf\n", id2, list_pop_back_float(table, id2));
+	printf("  List %zu front : %d\n", id4, list_pop_front_bool(table, id4));
+	printf("  List %zu back : %d\n", id4, list_pop_back_bool(table, id4));
+
+	print_list_table(table);
+
+	list_clear(table, id2);
+
+	printf("Empty/size:");
+	printf("  List %zu empty : %d\n", id3, list_empty(table, id3));
+	printf("  List %zu size : %zu\n", id3, list_size(table, id3));
+	printf("  List %zu empty : %d\n", id1, list_empty(table, id1));
+	printf("  List %zu size : %zu\n", id1, list_size(table, id1));
+	printf("  List %zu empty : %d\n", id2, list_empty(table, id2));
+	printf("  List %zu size : %zu\n", id2, list_size(table, id2));
+	printf("  List %zu empty : %d\n", id4, list_empty(table, id4));
+	printf("  List %zu size : %zu\n", id4, list_size(table, id4));
+
+	print_list_table(table);
+	
+	list_rm_reference(table, id1);
+	list_deallocate(table, id3);
+
+	print_list_table(table);
 	delete_list_table(table);
 	// struct array_table* table = create_array_table();
 
